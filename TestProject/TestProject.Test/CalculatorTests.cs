71fdaafeb35802d6c36bb092c367716707350c0e
manupawlow <manupaw2000@gmail.com>
2023-09-02 16:44:58 -0300

    Feature/add calculator (#2)
    * create project
    
    * initial scaffolding
    
    * add division

---
+++ TestProject/TestProject.Test/CalculatorTests.cs
@@ -0,0 +1,21 @@
+namespace TestProject.Test
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _sut;
+
+        [Fact]
+        public void Add_Should_ReturnSum()
+        {
+            var result = _sut.Add(1, 1);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Divide_Should_ReturnDivision()
+        {
+            var result = _sut.Divide(1, 1);
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ TestProject/TestProject/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace TestProject
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
