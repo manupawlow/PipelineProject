92e9f033284dd384f9c0d3dad40858c8470ccf8b
manupawlow <manupaw2000@gmail.com>
2023-09-02 16:51:51 -0300

    asdasd

--- TestProject/TestProject/Calculator.cs
+++ TestProject/TestProject/Calculator.cs
@@ -4,8 +4,14 @@
     {
         public int Add(int a, int b)
         {
+            var pepe = "unused";
             return a + b;
         }
+
+        //public int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
 
         public int Divide(int a, int b)
         {
