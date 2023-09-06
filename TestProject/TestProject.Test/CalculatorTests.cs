6260bc7295cb08bc6b68478dedabcf185140af9f
manupawlow <manupaw2000@gmail.com>
2023-09-06 13:11:54 -0300

    add blank

--- TestProject/TestProject.Test/CalculatorTests.cs
+++ TestProject/TestProject.Test/CalculatorTests.cs
@@ -5,14 +5,14 @@ namespace TestProject.Test
         private readonly Calculator _sut = new();
 
         [Fact]
-        public void Add_Should_ReturnSum()
-        {
+        public void Add_Should_ReturnSum() {
             var result = _sut.Add(1, 1);
             Assert.Equal(2, result);
         }
-          
-        [Fact]    
-        public void Divide_Should_ReturnDivision() {
+
+        [Fact]
+        public void Divide_Should_ReturnDivision()
+        {
             var result = _sut.Divide(1, 1);
             Assert.Equal(1, result);
         }
