f634dd3a88af2f8c9929d0ff6466259fc9882641
manupawlow <manupaw2000@gmail.com>
2023-09-06 20:14:01 -0300

    bla bla bla 2

--- TestProject/TestProject/Calculator.cs
+++ TestProject/TestProject/Calculator.cs
@@ -11,5 +11,15 @@
         {
             return a / b;
         }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int MultiplyBy(int a, int b)
+        {
+            return a * b;
+        }
     }
 }
