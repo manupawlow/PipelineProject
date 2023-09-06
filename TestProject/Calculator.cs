e3ef2cc86ccb462003cbf2a1e0f67c75723946a7
manupawlow <manupaw2000@gmail.com>
2023-09-06 11:45:04 -0300

    asdasd (#3)
    * asdasd
    
    * Update dotnet-format.yml
    
    * remove unuse
    
    * Create codeql.yml
    
    * aaaa
    
    * Update build-and-test.yml
    
    * Update super-linter.yml
    
    * Update README.md

---
+++ TestProject/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace TestProject;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        return a / b;
+    }
+}
