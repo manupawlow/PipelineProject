da3c19ab6523f6ae0e9510588f3ecce7593c592f
manupawlow <manupaw2000@gmail.com>
2023-09-16 12:15:01 -0300

    Update Calculator.cs

--- TestProject/TestProject/Calculator.cs
+++ TestProject/TestProject/Calculator.cs
@@ -6,7 +6,7 @@ namespace TestProject
     {
         public int Add(int a, int b)
         {
-            return a + b;
+            return a + b + 1;
         }
 
         //public int Add2(int a, int b) {
